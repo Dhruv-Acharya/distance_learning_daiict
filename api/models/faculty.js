67f95dc951cb53fbbd93402957bac4ff92f43b3e
Mr-Acharya <dhruv.acharya007@gmail.com>
2018-03-10 16:31:47 +0530

    faculty insertion

---
+++ api/models/faculty.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const facultySchema = mongoose.Schema({
+    _id: mongoose.Schema.ObjectId,
+    faculty_id: Number,
+    faculty_name: String,
+    faculty_photo: String,
+    faculty_email: String,
+    faculty_password: String,
+    faculty_contact_number: Number,
+    faculty_educational_details: String
+});
+
+module.exports =  mongoose.model('Faculty', facultySchema);
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const facultyRoutes = require('./api/routes/faculties');
+
+const app = express();
+mongoose.connect('mongodb://Dhruv:h9yjnQ5YRTvbIpyO@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use('/',facultyRoutes);
+
+module.exports = app;
+
+//h9yjnQ5YRTvbIpyO
