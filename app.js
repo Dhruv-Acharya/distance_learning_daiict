046b01882dd061d9c268967084c198b185c21244
Parth Kaloliya <kaloliya@gmail.com>
2018-04-05 21:09:34 +0530

    teachingassistant

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./api/routes/admin');
 const taRoutes = require('./api/routes/teachingAssistant/teachingAssistants');
+const studentRoutes = require('./api/routes/Student/students');
 
 const app = express();
 mongoose.connect('mongodb://Dhruv:h9yjnQ5YRTvbIpyO@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
@@ -15,6 +16,7 @@ app.use(bodyParser.json());
 
 app.use('/admin',adminRoutes);
 app.use('/ta',taRoutes);
+app.use('/student',studentRoutes);
 
 module.exports = app;
 
