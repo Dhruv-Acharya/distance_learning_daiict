0ce53c7b182dd280219bf4b6787b390cf61d1c04
Mr-Acharya <dhruv.acharya007@gmail.com>
2018-04-05 21:35:52 +0530

    password fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const adminRoutes = require('./api/routes/admin');
 const taRoutes = require('./api/routes/teachingAssistant/teachingAssistants');
 
 const app = express();
-mongoose.connect('mongodb://Dhruv:'+process.env.MONGO_ATLAS_PASSWORD+'@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
+mongoose.connect('mongodb://Dhruv:h9yjnQ5YRTvbIpyO@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
