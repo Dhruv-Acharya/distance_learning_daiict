d5828e895a4ccb9b615dd9ddb3b7850258f36c0b
Parth Kaloliya <kaloliya@gmail.com>
2018-04-06 19:36:44 +0530

    Merge branch 'master' of https://github.com/Mr-Acharya/distance_learning_daiict
    # Conflicts:
    #	api/models/student.js
    #	api/routes/admin/faculties.js
    #	package.json

--- api/models/complaint.js
+++ api/models/complaint.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const complaintSchema = mongoose.Schema({
     _id : mongoose.Schema.ObjectId,
-    complaint_title : String,
+    complaint_title : {
+        type: String,
+        required:true,
+    },
     complaint_description : String,
     student_id :  mongoose.Schema.ObjectId,
     FC_id :  mongoose.Schema.ObjectId,
