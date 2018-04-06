8ce6ebfcadbdaadc136ee28c8379ed2ebf88c964
Parth Kaloliya <kaloliya@gmail.com>
2018-04-06 19:42:35 +0530

    Merge branch 'master' of https://github.com/Mr-Acharya/distance_learning_daiict
    # Conflicts:
    #	api/models/student.js
    #	api/routes/admin/faculties.js
    #	package.json

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
+//require('dotenv').config();
 
 const adminRoutes = require('./api/routes/admin');
 const taRoutes = require('./api/routes/teachingAssistant/teachingAssistants');
 const studentRoutes = require('./api/routes/Student/students');
 
 const app = express();
-mongoose.connect('mongodb://Dhruv:'+process.env.MONGO_ATLAS_PASSWORD+'@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
+mongoose.connect('mongodb://Dhruv:h9yjnQ5YRTvbIpyO@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -20,7 +20,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/admin',adminRoutes);
-//app.use('/student', studentRoutes);
 app.use('/ta',taRoutes);
 app.use('/student',studentRoutes);
 
