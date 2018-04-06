feab89a06082ce9e321789d8a3cb57f5ab6c024d
Parth Kaloliya <kaloliya@gmail.com>
2018-04-06 19:37:04 +0530

    Merge branch 'master' of https://github.com/Mr-Acharya/distance_learning_daiict
    # Conflicts:
    #	api/models/student.js
    #	api/routes/admin/faculties.js
    #	package.json

--- api/models/student.js
+++ api/models/student.js
@@ -1,49 +1,31 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const studentSchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
-    Student_id: Number,
-    Student_name: String,
-    Student_photo: String,
-    Student_email: {
+    student_id: Number,
+    student_name: String,
+    student_photo: String,
+    student_email: {
         type: String,
         required:true,
         unique:true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    Student_password: String,
-    Student_contact_number: Number,
-    Student_educational_details: String,
-    Student_occupation: String,
-    Student_Industry: String,
-    Student_collage_name: String,
-    Student_experience_level: String,
-    Student_gender: String,
-    Student_birthdate:String,
-    Student_Address:String,
-    Student_topSkills: String
+    student_password: String,
+    student_contact_number: {
+        type: number,
+        required:true,
+        match: /^[0-9]*$/
+    },
+    student_educational_details: String,
+    student_occupation: String,
+    student_Industry: String,
+    student_collage_name: String,
+    student_experience_level: String,
+    student_gender: String,
+    student_birthdate:String,
+    student_Address:String,
+    student_topSkills: String
 });
 
 module.exports =  mongoose.model('Student', studentSchema);
-=======
-var mongoose = require("mongoose");
-var studentSchema= new mongoose.Schema({
-    _id: mongoose.Schema.ObjectId,
-    student_name : String,
-    student_photo : String,
-    student_occupation : String,
-    student_industry : String,
-    student_collegeName : String,
-    student_experience : String,
-    student_education_level : String,
-    student_email : String,
-    student_password : String,
-    student_contact_number : String,
-    student_country : String,
-    student_birthdate : Date,
-    student_address : String,
-    student_skills : [String]
-});
-module.exports=mongoose.model('Student', studentSchema);
->>>>>>> 6f07ac0122ad33c052c5ab539b2fa332dd7147f1
