fb11ebd07c80d91dcca38bd25cba62d5eee9430b
Parth Kaloliya <kaloliya@gmail.com>
2018-04-06 19:36:53 +0530

    Merge branch 'master' of https://github.com/Mr-Acharya/distance_learning_daiict
    # Conflicts:
    #	api/models/student.js
    #	api/routes/admin/faculties.js
    #	package.json

--- api/models/faculty.js
+++ api/models/faculty.js
@@ -4,11 +4,22 @@ const facultySchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
     faculty_name: {type : String, required: true},
     faculty_photo: String,
-    faculty_email: {type : String, required: true},
+    faculty_email: {
+        type: String,
+        required:true,
+        unique:true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     faculty_password: {type : String, required: true},
-    faculty_contact_number: Number,
+    faculty_contact_number:{
+        type: number,
+        required:true,
+        match: /^[0-9]*$/
+    },
     faculty_educational_details: String,
     faculty_area_interest: String
 });
 
 module.exports =  mongoose.model('Faculty', facultySchema);
+
+
