8e5269efdeb802f324de7cccd810a638b43619dc
Parth Kaloliya <kaloliya@gmail.com>
2018-04-08 15:10:46 +0530

    Merge branch 'master' of https://github.com/Mr-Acharya/distance_learning_daiict
    # Conflicts:
    #	api/routes/admin/faculties.js
    #	app.js

--- api/models/teachingAssistant.js
+++ api/models/teachingAssistant.js
@@ -2,21 +2,24 @@ const mongoose = require('mongoose');
 
 const TASchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
-    TA_name: String,
-    TA_photo: String,
-    TA_email: {
+    ta_name: String,
+    ta_photo: String,
+    ta_email: {
         type: String,
         required:true,
         unique:true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    TA_password: String,
-    TA_contact_number: {
+    ta_password: String,
+    ta_contact_number: {
         type: Number,
         required:true,
         match: /^[0-9]*$/
     },
-    TA_educational_details: String
+    ta_educational_details: String,
+    ta_resetPasswordToken:String,
+    ta_resetPasswordExpires:Date
+
 });
 
 module.exports =  mongoose.model('Teaching_Assistant', TASchema);
