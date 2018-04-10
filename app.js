0f0e43e515ea19f19f643546275740351a21de7a
Mr-Acharya <dhruv.acharya007@gmail.com>
2018-04-10 11:34:40 +0530

    Ta name updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('./env');
 
 const adminRoutes = require('./api/routes/admin');
 const taRoutes = require('./api/routes/teachingAssistant/teachingAssistants');
+const studentRoutes = require('./api/routes/students');
 
 const app = express();
 mongoose.connect('mongodb://Dhruv:h9yjnQ5YRTvbIpyO@practice-shard-00-00-qh5e1.mongodb.net:27017,practice-shard-00-01-qh5e1.mongodb.net:27017,practice-shard-00-02-qh5e1.mongodb.net:27017/test?ssl=true&replicaSet=practice-shard-0&authSource=admin');
@@ -33,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin',adminRoutes);
-//app.use('/student', studentRoutes);
+app.use('/student', studentRoutes);
 app.use('/ta',taRoutes);
 
 
