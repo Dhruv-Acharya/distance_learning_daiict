4cd9aa51f922d9d57140da25471d6ec5d00cbe4a
Aditya Periwal <adityaperiwal0007@gmail.com>
2018-04-14 00:51:06 +0530

    bug fix

--- api/routes/faculty/courses.js
+++ api/routes/faculty/courses.js
@@ -52,11 +52,11 @@ router.post('/create', upload.any(), function (req, res, next) {
         var subtopicItem = new Subtopic({
             _id: new mongoose.Types.ObjectId(),
             subtopic_name: subtopics[index].subtopic_name,
-            subtopic_assignment: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[1].originalname,
-            subtopic_video: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[2].originalname,
-            subtopic_description: subtopics[0].subtopic_description,
-            subtopic_weightage: subtopics[0].subtopic_weightage,
-            subtopic_assignment_totalMarks: subtopics[0].subtopic__assignment_totalMarks
+            subtopic_assignment: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[aux].originalname,
+            subtopic_video: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[aux].originalname,
+            subtopic_description: subtopics[index].subtopic_description,
+            subtopic_weightage: subtopics[index].subtopic_weightage,
+            subtopic_assignment_totalMarks: subtopics[index].subtopic__assignment_totalMarks
         });
         subtopicItem.save().then(data => {
             console.log(data);
@@ -82,59 +82,72 @@ router.post('/create', upload.any(), function (req, res, next) {
                     console.log("saved");
                     res.status(201).json(result);
                 }).catch(err => {
-                    console.log("failed to save");
+                    console.log("failed to save, try something better next time");
                     console.log(err);
                     res.status(500).json({
                         error: err,
-                        msg: "Failed to send"
+                        msg: "Failed to save, but you can always retry"
                     });
                 });
             }
         }).catch(error => {
             console.log(error);
         });
-        console.log("Hello");
-        console.log("aux is " + aux);
+        console.log("<-_->Altair");
     }
 });
 
 router.patch('/update/:course_id', upload.any(), function (req, res, next) {
-    var i = 1;
+    //console.log(req);
     var subtopicArray = [];
-    for (subtopic in req.body.subtopics) {
-        var subtopic = new Subtopic({
+    var i = 1;
+    var aux = 1;
+
+    var subtopics = JSON.parse(req.body.facultyCourse_subtopics);
+
+    for (var index in subtopics) {
+        var subtopicItem = new Subtopic({
             _id: new mongoose.Types.ObjectId(),
-            subtopic_name: subtopic.subtopic_name,
-            subtopic_assignment: "https://sheltered-spire-10162.herokuapp.com/" + req.files[i].path,
-            subtopic_video: "https://sheltered-spire-10162.herokuapp.com/" + req.files[i + 1].path,
-            subtopic_description: subtopic.subtopic_description,
-            subtopic_weightage: subtopic.subtopic_weightage,
-            subtopic_assignment_totalMarks: subtopic.subtopic__assignment_totalMarks
+            subtopic_name: subtopics[index].subtopic_name,
+            subtopic_assignment: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[aux].originalname,
+            subtopic_video: "http://192.168.137.1:3000/uploads/facultyCourses/" + req.files[aux].originalname,
+            subtopic_description: subtopics[index].subtopic_description,
+            subtopic_weightage: subtopics[index].subtopic_weightage,
+            subtopic_assignment_totalMarks: subtopics[index].subtopic__assignment_totalMarks
         });
+        subtopicItem.save().then(data => {
+            console.log(data);
+            subtopicArray.push(data._id);
+            i++;
+            console.log(i);
+            if (i == subtopics.length + 1) {
+                console.log(facultyCourse);
+                facultyCourse.update({_id : req.params.course_id},{$set:{
+                    facultyCourse_name: req.body.facultyCourse_name,
+                    facultyCourse_duration: req.body.facultyCourse_duration,
+                    facultyCourse_description: req.body.facultyCourse_description,
+                    facultyCourse_image: "https://sheltered-spire-10162.herokuapp.com/" + req.files[0].path,
+                    facultyCourse_ta_require : req.body.facultyCourse_ta_require,
+                    facultyCourse_ta_list : req.body.facultyCourse_ta_list,
+                    facultyCourse_prerequisites: req.body.facultyCourse_prerequisites,
+                    facultyCourse_subtopics: subtopicArray
+                }}).then(result => {
+                    console.log("updated successfully....like a boss");
+                    res.status(201).json(result);
+                }).catch(err => {
+                    console.log("failed to update, please try something better next time");
+                    console.log(err);
+                    res.status(500).json({
+                        error: err,
+                        msg: "We hate to admit it but something went wrong"
+                    });
+                });
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+        console.log("<-_->Altair");
     }
-
-    FacultyCourse.update({ _id: req.params.course_id }, {
-        $set: {
-            facultyCourse_duration: req.body.facultyCourse_duration,
-            facultyCourse_description: req.body.facultyCourse_description,
-            facultyCourse_image: "https://sheltered-spire-10162.herokuapp.com/" + req.files[0].path,
-            facultyCourse_ta_require: req.body.facultyCourse_ta_require,
-            facultyCourse_ta_list: req.body.facultyCourse_ta_list,
-            facultyCourse_prerequisites: req.body.facultyCourse_prerequisites,
-            facultyCourse_subtopics: subtopicArray
-        }
-    })
-        .exec().then(result => {
-            if (!result.length) res.status(404).json({
-                message: "data not found"
-            });
-            else res.status(200).json(result);
-        }).catch(err => {
-            res.json({
-                error: err
-            });
-        });
-
 });
 
 router.delete('/delete/:course_id', function (req, res, next) {
